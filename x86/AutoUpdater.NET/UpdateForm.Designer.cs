1acab64c1733a2e42a3a473eccc3f749dad1ac9f
franco28 <franco.mato.contin@gmail.com>
2020-08-22 11:19:07 -0300

    Updated for MotoBootLogoMaker

---
+++ x86/AutoUpdater.NET/UpdateForm.Designer.cs
@@ -0,0 +1,114 @@
+﻿using System.Threading;
+
+namespace AutoUpdaterDotNET
+{
+    partial class UpdateForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UpdateForm));
+            this.pictureBoxIcon = new System.Windows.Forms.PictureBox();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.labelUpdate = new System.Windows.Forms.Label();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.labelReleaseNotes = new System.Windows.Forms.Label();
+            this.webBrowser = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxIcon
+            // 
+            this.pictureBoxIcon.Image = global::AutoUpdaterDotNET.Properties.Resources.update;
+            resources.ApplyResources(this.pictureBoxIcon, "pictureBoxIcon");
+            this.pictureBoxIcon.Name = "pictureBoxIcon";
+            this.pictureBoxIcon.TabStop = false;
+            // 
+            // buttonUpdate
+            // 
+            resources.ApplyResources(this.buttonUpdate, "buttonUpdate");
+            this.buttonUpdate.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonUpdate.Image = global::AutoUpdaterDotNET.Properties.Resources.download_32;
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.ButtonUpdateClick);
+            // 
+            // labelUpdate
+            // 
+            resources.ApplyResources(this.labelUpdate, "labelUpdate");
+            this.labelUpdate.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.labelUpdate.Name = "labelUpdate";
+            // 
+            // labelDescription
+            // 
+            resources.ApplyResources(this.labelDescription, "labelDescription");
+            this.labelDescription.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.labelDescription.Name = "labelDescription";
+            // 
+            // labelReleaseNotes
+            // 
+            resources.ApplyResources(this.labelReleaseNotes, "labelReleaseNotes");
+            this.labelReleaseNotes.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(180)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.labelReleaseNotes.Name = "labelReleaseNotes";
+            // 
+            // webBrowser
+            // 
+            resources.ApplyResources(this.webBrowser, "webBrowser");
+            this.webBrowser.BackColor = System.Drawing.Color.White;
+            this.webBrowser.ForeColor = System.Drawing.Color.Black;
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.ReadOnly = true;
+            // 
+            // UpdateForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.labelReleaseNotes);
+            this.Controls.Add(this.labelDescription);
+            this.Controls.Add(this.labelUpdate);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.pictureBoxIcon);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "UpdateForm";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.UpdateForm_FormClosed);
+            this.Load += new System.EventHandler(this.UpdateFormLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBoxIcon;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Label labelUpdate;
+        private System.Windows.Forms.Label labelDescription;
+        private System.Windows.Forms.Label labelReleaseNotes;
+        private System.Windows.Forms.RichTextBox webBrowser;
+    }
+}
---
+++ x86/AutoUpdater.NET/UpdateForm.cs
@@ -0,0 +1,101 @@
+﻿
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace AutoUpdaterDotNET
+{
+    internal partial class UpdateForm : Form
+    {
+        private readonly SettingsMng oConfigMng = new SettingsMng();
+        private readonly UpdateInfoEventArgs _args;
+
+        public UpdateForm(UpdateInfoEventArgs args)
+        {
+            _args = args;
+
+            InitializeComponent();
+
+            oConfigMng.LoadConfig();
+            if (oConfigMng.Config.ToolTheme == "light")
+            {
+                this.BackColor = Color.FromArgb(250, 232, 232);  
+                this.ForeColor = Color.FromArgb(38, 38, 38);
+
+                webBrowser.BackColor = labelReleaseNotes.BackColor = labelDescription.BackColor = labelUpdate.BackColor = pictureBoxIcon.BackColor = Color.FromArgb(250, 232, 232);
+                webBrowser.ForeColor = labelReleaseNotes.ForeColor = labelDescription.ForeColor = labelUpdate.ForeColor = Color.FromArgb(38, 38, 38);
+
+                buttonUpdate.FlatStyle = FlatStyle.Standard;
+                buttonUpdate.BackColor = Color.FromArgb(255, 255, 255);
+                buttonUpdate.ForeColor = Color.FromArgb(38, 38, 38);
+            }
+            if (oConfigMng.Config.ToolTheme == "dark")
+            {
+                this.BackColor = Color.FromArgb(38, 38, 38);
+                this.ForeColor = Color.FromArgb(250, 232, 232);
+
+                webBrowser.BackColor = labelReleaseNotes.BackColor = labelDescription.BackColor = labelUpdate.BackColor = pictureBoxIcon.BackColor = Color.FromArgb(38, 38, 38);
+                webBrowser.ForeColor = labelReleaseNotes.ForeColor = labelDescription.ForeColor = labelUpdate.ForeColor = Color.FromArgb(250, 232, 232);
+
+                buttonUpdate.FlatStyle = FlatStyle.Flat;
+                buttonUpdate.BackColor = Color.FromArgb(38, 38, 38);
+                buttonUpdate.ForeColor = Color.FromArgb(250, 232, 232);
+            }
+            var resources = new System.ComponentModel.ComponentResourceManager(typeof(UpdateForm));
+            Text = string.Format(resources.GetString("$this.Text", CultureInfo.CurrentCulture),
+                AutoUpdater.AppTitle, _args.CurrentVersion);
+            labelUpdate.Text = string.Format(resources.GetString("labelUpdate.Text", CultureInfo.CurrentCulture),
+                AutoUpdater.AppTitle);
+            labelDescription.Text =
+                string.Format(resources.GetString("labelDescription.Text", CultureInfo.CurrentCulture),
+                    AutoUpdater.AppTitle, _args.CurrentVersion, _args.InstalledVersion);
+
+            if (AutoUpdater.Mandatory && AutoUpdater.UpdateMode == Mode.Forced)
+            {
+                ControlBox = false;
+            }
+        }
+
+        private void UpdateFormLoad(object sender, EventArgs e)
+        {
+            var webRequest = WebRequest.Create(@"https://raw.githubusercontent.com/Franco28/MotoBootLogoMaker/master/x86/Setup/changelog.txt");
+            using (var response = webRequest.GetResponse())
+            using (var content = response.GetResponseStream())
+            using (var reader = new StreamReader(content))
+            {
+                var strContent = reader.ReadToEnd();
+                webBrowser.Text = strContent.ToString();
+            }
+            var labelSize = new Size(Width - 110, 0);
+            labelDescription.MaximumSize = labelUpdate.MaximumSize = labelSize;
+        }
+
+        private void ButtonUpdateClick(object sender, EventArgs e)
+        {
+            if (AutoUpdater.OpenDownloadPage)
+            {
+                var processStartInfo = new ProcessStartInfo(_args.DownloadURL);
+
+                Process.Start(processStartInfo);
+
+                DialogResult = DialogResult.OK;
+            }
+            else
+            {
+                if (AutoUpdater.DownloadUpdate(_args))
+                {
+                    DialogResult = DialogResult.OK;
+                }
+            }
+        }
+
+        private void UpdateForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            AutoUpdater.Running = false;
+        }
+    }
+}
